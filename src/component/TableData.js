6ab384671bf6ab62c5074b124fc753675be6ae24
chaman0303 <chamangupta0303@gmail.com>
2024-02-28 23:09:34 +0530

    updates in calender

--- src/component/TableData.js
+++ src/component/TableData.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import dayjs from "dayjs";
 
 export function TableData() {
   const [data, setData] = useState(null);
@@ -17,63 +18,60 @@ export function TableData() {
   if (!data) return null;
 
   // console.log(data.appointments,"data")
+
   return (
     <>
-      <div className="container mx-auto px-4 sm:px-8">
+      <div className="container px-4 mx-auto sm:px-8">
         <div className="py-4">
           <div>
             <h2 className="text-2xl font-semibold leading-tight text-gray-400">
               Today's Appointment List
             </h2>
           </div>
-          <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-            <div className="inline-block min-w-full shadow-md rounded-lg overflow-hidden">
+          <div className="px-4 py-4 -mx-4 overflow-x-auto sm:-mx-8 sm:px-8">
+            <div className="inline-block min-w-full overflow-hidden rounded-lg shadow-md">
               <table className="min-w-full leading-normal">
                 <thead>
                   <tr>
-                    <th className="px-5 py-6 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-bold text-gray-400 uppercase tracking-wider">
+                    <th className="px-5 py-6 text-xs font-bold tracking-wider text-left text-gray-400 uppercase bg-gray-100 border-b-2 border-gray-200">
                       Patients
                     </th>
-                    <th className="px-5 py-6 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-bold text-gray-400 uppercase tracking-wider">
+                    <th className="px-5 py-6 text-xs font-bold tracking-wider text-left text-gray-400 uppercase bg-gray-100 border-b-2 border-gray-200">
                       Date
                     </th>
-                    <th className="px-5 py-6 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-bold text-gray-400 uppercase tracking-wider">
+                    <th className="px-5 py-6 text-xs font-bold tracking-wider text-left text-gray-400 uppercase bg-gray-100 border-b-2 border-gray-200">
                       Time
                     </th>
-                    <th className="px-5 py-6 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-bold text-gray-400 uppercase tracking-wider">
+                    <th className="px-5 py-6 text-xs font-bold tracking-wider text-left text-gray-400 uppercase bg-gray-100 border-b-2 border-gray-200">
                       Doctor
                     </th>
-                    <th className="px-5 py-6 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-bold text-gray-400 uppercase tracking-wider">
+                    <th className="px-5 py-6 text-xs font-bold tracking-wider text-left text-gray-400 uppercase bg-gray-100 border-b-2 border-gray-200">
                       Injury
                     </th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-bold text-gray-400 uppercase tracking-wider">
+                    <th className="px-5 py-3 text-xs font-bold tracking-wider text-left text-gray-400 uppercase bg-gray-100 border-b-2 border-gray-200">
                       Action
                     </th>
                   </tr>
                 </thead>
                 <tbody>
-                  {data.map((element,index) => (
+                  {data?.map((element,index) => (
                     <tr key={index}>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                      <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
                         <div className="flex">
-                          <div className="flex-shrink-0 w-10 h-10">
-                            <img
-                              className="w-full h-full rounded-full"
-                              src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
-                              alt=""
-                            />
+                          <div className="flex items-center justify-center flex-shrink-0 w-10 h-10 rounded-full bg-black/30">
+                            <p className="mb-1 text-2xl font-bold text-center text-white">{element?.patient_name.substring(0,1)}</p>
                           </div>
                           <div className="ml-3">
-                            <p className="text-gray-900 whitespace-no-wrap font-bold ">
+                            <p className="font-bold text-gray-900 whitespace-no-wrap ">
                               {element.patient_name}
                             </p>
-                            <p className="py-1 text-gray-600 whitespace-no-wrap text-xs font-light">
+                            <p className="py-1 text-xs font-light text-gray-600 whitespace-no-wrap">
                               {element.mobile_number}
                             </p>
                           </div>
                         </div>
                       </td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                      <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
                         <div className="flex">
                           <div className="flex-shrink-0 w-5 h-5">
                             <img
@@ -81,29 +79,29 @@ export function TableData() {
                               alt="calendar--v1"
                             />
                           </div>
-                          <div className="ml-3">
-                            <p className="text-gray-900 whitespace-no-wrap ">
-                              {element.appointment_date}
+                          <div className="ml-1">
+                            <p className="text-gray-900 whitespace-no-wrap">
+                              {dayjs(element.appointment_date).format('DD MMM, YYYY')}
                             </p>
                           </div>
                         </div>
                       </td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                        <div className="flex">
+                      <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                        <div className="flex items-center">
                           <div className="flex-shrink-0 w-5 h-5">
                             <img
                               src="https://img.icons8.com/windows/20/clock--v1.png"
                               alt="clock--v1"
-                            />{" "}
+                            />
                           </div>
-                          <div className="ml-3">
+                          <div className="ml-1">
                             <p className="text-gray-900 whitespace-no-wrap ">
                               {element.appointment_time}
                             </p>
                           </div>
                         </div>
                       </td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                      <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
                         <div className="flex">
                           <div className="flex-shrink-0 w-5 h-5">
                             <img
@@ -118,16 +116,16 @@ export function TableData() {
                           </div>
                         </div>
                       </td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                        <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                      <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                        <span className="relative inline-block px-3 py-1 font-semibold leading-tight text-green-900">
                           <span
                             aria-hidden
-                            className="absolute inset-0 bg-gray-200 opacity-50 rounded-lg "
+                            className="absolute inset-0 bg-gray-200 rounded-lg opacity-50 "
                           ></span>
                           <span className="relative">{element.injury}</span>
                         </span>
                       </td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-2xl">
+                      <td className="px-5 py-5 text-2xl bg-white border-b border-gray-200">
                         :
                       </td>
                     </tr>
